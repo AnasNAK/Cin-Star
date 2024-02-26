23b7b7eb2866c32bd94457c616d7773f12a7d86b
youssef lhassar <lhssaryoussef@gmail.com>
2024-02-26 16:12:18 +0100

    adding authentification with google & spatie

---
+++ Ceni-star-structure/app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class ProviderController extends Controller
+{
+
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $socialUser = Socialite::driver($provider)->user();
+
+        $user = User::updateOrCreate([
+            'provider_id' => $socialUser->id,
+            'provider' => $provider,
+        ], [
+            'name' => $socialUser->name,
+            'email' => $socialUser->email,
+            'provider_token' => $socialUser->token,
+
+        ]);
+
+        Auth::login($user);
+        return redirect('/dashboard');
+    }
+
+    /**
+
+  
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
