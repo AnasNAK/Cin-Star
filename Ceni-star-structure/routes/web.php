f57580db7900298548bb2f26157a481d6648e9ba
youssef <youssefhihi18@gmail.com>
2024-02-26 21:12:00 +0100

    add migration and code backend of search

--- Ceni-star-structure/routes/web.php
+++ Ceni-star-structure/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FilmController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [FilmController::class, 'search'])->name('films.search');
+  
